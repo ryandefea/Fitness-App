741beeec88494e0dccb52a1d82c5e83fa498dab4
ryandefea <ryan.defea@gmail.com>
2022-09-27 20:53:06 -0500

    update colors

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Box, Stack, Typography, Button } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+// import HeroBannerImage from '../assets/images/banner.png';
+ import HeroBannerImage from '../assets/images/LA.jpg';
 
 const HeroBanner = () => {
   return (
@@ -9,9 +10,9 @@ const HeroBanner = () => {
       mt: { lg: '212px', xs:'70px'}, 
       ml: { sm: '50px'}
     }} position="relative" p="20px">
-      <Typography color="#FF2625"
-      fontWeight="600" fontSize="26px">
-        Fitness Club
+      <Typography color="#0000FF"
+      fontWeight="600" fontSize="60px">
+        Larry's Gym
       </Typography>
       <Typography fontWeight={700}
       sx={{ fontSize: { lg: '44px', xs: '40px'}
@@ -22,7 +23,7 @@ const HeroBanner = () => {
       <Typography fontSize="22px" lineHeight="35px" mb={5}>
         How do you get big?
       </Typography>
-      <Button variant="contained" color="error" href="#exercises" sx={{ backgroundColor: '#ff2625', padding: '10px'}}>Explore Exercises</Button>
+      <Button variant="contained" color="error" href="#exercises" sx={{ backgroundColor: '#0000FF', padding: '10px'}}>Explore Exercises</Button>
       <Typography fontWeight={600} color="#ff2625"
       sx={{opacity: 0.1, display: {lg: 'block', xs: 'none'}}} fontSize="200px">
         Lift
