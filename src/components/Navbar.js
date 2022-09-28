bfaf06d7072f6ea998f321581cad3fc33915084b
ryandefea <ryan.defea@gmail.com>
2022-09-27 20:53:59 -0500

    update bottom border color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
           fontSize="24px"
           alignItems="flex-end"
           >
-          <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
+          <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #0000FF'}}>Home</Link>
           <a href="#exercises" style={
             {textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
         </Stack>
